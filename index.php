91637f1f5197f0786e5c5c73bdcd5faebee2161e
Replit user <>
2022-08-04 00:20:21 +0000

    safe keeping

--- index.php
+++ index.php
@@ -2,19 +2,59 @@
 <head>
 <title>Bond Web Service Demo</title>
 <style>
-	body {font-family:georgia;}
+	body {
+    font-family:georgia;
+  }
+
+  .film{
+    border:1px solid #E77DC2;
+    border-radius: 5px;
+    padding: 5px;
+    margin-bottom:5px;
+    position:relative;   
+  }
+ 
+  .pic{
+    position:absolute;
+    right:10px;
+    top:10px;
+  }
+ 
+  .pic img{
+	max-width:50px;
+  }
+
 </style>
 <script src="https://code.jquery.com/jquery-latest.js"></script>
 
 <script type="text/javascript">
-$(document).ready(function() {  
+$(document).ready(function() { 
+ 
+ $('.category').click(function(e){
+   e.preventDefault(); //stop default action of the link
+   cat = $(this).attr("href");  //get category from URL
+  
+   var request = $.ajax({
+     url: "api.php?cat=" + cat,
+     method: "GET",
+     dataType: "json"
+   });
+   request.done(function( data ) {
+     console.log(data);
 
-	$('.category').click(function(e){
-        e.preventDefault(); //stop default action of the link
-		cat = $(this).attr("href");  //get category from URL
-		alert(cat);
-	});
-});	
+     let myData = JSON.stringify(data,null,4);
+      myData = "<pre>" + myData + "</pre>";
+     $("#output").html(myData);
+     
+     
+   });
+   request.fail(function(xhr, status, error ) {
+alert('Error - ' + xhr.status + ': ' + xhr.statusText);
+   });
+ 
+  });
+}); 
+	
 
 
 </script>
@@ -25,7 +65,19 @@ $(document).ready(function() {
 		<a href="box" class="category">Bond Films By International Box Office Totals</a>
 		<h3 id="filmtitle">Title Will Go Here</h3>
 		<div id="films">
-			<p>Films will go here</p>
+      <div class="film">
+        <b>Film</b>: 1<br />
+        <b>Title</b>: Dr. No<br />
+        <b>Year</b>: 1962<br />
+        <b>Director</b>: Terence Young<br />
+        <b>Producers</b>: Harry Saltzman and Albert R. Broccoli<br />
+        <b>Writers</b>: Richard Maibaum, Johanna Harwood and Berkely Mather<br />
+        <b>Composer</b>: Monty Norman<br />
+        <b>Bond</b>: Sean Connery<br />
+        <b>Budget</b>: $1,000,000.00<br />
+        <b>BoxOffice</b>: $59,567,035.00<br />
+        <div class="pic"><img src="thumbnails/dr-no.jpg"/></div>
+      </div>
 		</div>
 		<div id="output">Results go here</div>
 	</body>
